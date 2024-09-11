6dab8cbafd6e303115049dc86e2ae7740210e26d
FelipeGuerraInfante <felipe.guerra2101@alumnos.ubiobio.cl>
2024-09-10 21:33:46 -0300

    ADD Ejercicio1

---
+++ VariablesyTiposdeDatos.js
@@ -0,0 +1,11 @@
+const nombre = "Maximiliano"
+const edad = 17
+const puedoConducir = false
+
+console.log("Nombre:" + nombre)
+console.log("Edad:" + edad)
+console.log("¿Puede conducir?" + puedoConducir)
+
+console.log("Tipo variable nombre: " + typeof nombre)
+console.log("Tipo variable edad: " + typeof edad)
+console.log("Tipo variable puedoConducir: " + typeof puedoConducir)
