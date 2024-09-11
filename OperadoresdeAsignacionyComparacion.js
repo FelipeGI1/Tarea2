81c33c6ab927e9a7d301986ba6a09ae76c4d07ef
FelipeGuerraInfante <felipe.guerra2101@alumnos.ubiobio.cl>
2024-09-10 22:24:04 -0300

    ADD Ejercicio3

---
+++ OperadoresdeAsignacionyComparacion.js
@@ -0,0 +1,10 @@
+const numero1 = 15;
+const numero2 = 20;
+const numero3 = '25';
+
+console.log("¿n1 es mayor o igual a n2? " + (numero1 >= numero2))
+console.log("¿n1 es menor o igual a n2? " + (numero1 <= numero2))
+console.log("¿n1 es menor que n3? " + (numero1 < numero3))
+console.log("¿n3 es menor que n2? " + (numero3 < numero2))
+console.log("¿n3 es estrictamente diferente a n1? " + (numero3 !== numero1))
+console.log("¿n1 es estrictamente igual a n2? " + (numero1 === numero2))
