4e296e618874bbf5a3f0da92be692a459130982d
FelipeGuerraInfante <felipe.guerra2101@alumnos.ubiobio.cl>
2024-09-10 22:41:35 -0300

    ADD Ejercicio4

---
+++ Condicionales.js
@@ -0,0 +1,55 @@
+const numero1 = 10;
+const numero2 = 20;
+const numero3 = 30;
+
+if(numero1 > numero2 && numero1 > numero3){
+    console.log("n1 es el mayor")
+}else if(numero2 > numero3){
+    console.log("n2 es el mayor")
+}else{
+    console.log("n3 es el mayor")
+}
+
+if(numero1 < numero2 && numero1 < numero3){
+    console.log("n1 es el menor")
+}else if(numero2 < numero3){
+    console.log("n2 es el menor")
+}else{
+    console.log("n3 es el menor")
+}
+
+if(numero1 % 2 === 0){
+    console.log("n1 es par")
+}else{
+    console.log("n1 es impar")
+}
+
+if(numero2 % 2 === 0){
+    console.log("n2 es par")
+}else{
+    console.log("n2 es impar")
+}
+
+if(numero3 % 2 === 0){
+    console.log("n3 es par")
+}else{
+    console.log("n3 es impar")
+}
+
+if(numero1 % 5 === 0){
+    console.log("n1 es múltiplo de 5")
+}else{
+    console.log("n1 no es múltiplo de 5")
+}
+
+if(numero2 % 5 === 0){
+    console.log("n2 es múltiplo de 5")
+}else{
+    console.log("n2 no es múltiplo de 5")
+}
+
+if(numero3 % 5 === 0){
+    console.log("n3 es múltiplo de 5")
+}else{
+    console.log("n3 no es múltiplo de 5")
+}
